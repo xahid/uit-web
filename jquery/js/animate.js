b5274766d8b1d885dc8ac25728e8d05523154568
xahid Ali Siddiqui <sh.xahidali@gmail.com>
2019-09-22 21:18:20 +0500

    class 22-9-2019

---
+++ jquery/js/animate.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+			$('.box').click(function(){
+
+			$(this).animate({"margin-top":"100px"});
+			$(this).animate({"margin-left":"100px"});
+			$(this).animate({"margin-top":"0"});
+			$(this).animate({"margin-left":"0"});	
+
+
+
+			});
+
+
+		});
